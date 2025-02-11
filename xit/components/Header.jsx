6216f739201d622f98f2e8272b6d622f62bb7e38
danil <daniachmetshin@gmail.com>
2025-02-11 18:24:36 +0200

    basic layout is initialized

---
+++ xit/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Header({ navigation }) {
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.header}>
+        <Text style={styles.title}>My App</Text>
+        <TouchableOpacity onPress={() => navigation.openDrawer()}>
+          <MaterialIcons name="menu" size={28} color="#EEEEEE" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: "#393E46",
+  },
+  header: {
+    height: 60,
+    backgroundColor: "#393E46",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  title: {
+    color: "#EEEEEE",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
